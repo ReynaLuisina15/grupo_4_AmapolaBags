eb14b174df2a462d4d2c0e2c64383fef48bc84eb
Meli Gimenez <melinagimenez1999@gmail.com>
2022-07-13 18:28:26 -0300

    add more estructure

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 app.listen(port, () => console.log(`Server reunning in http://localhost:${port}`));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
