2f86aa28670c12ee98d3d9f776580602d76a8a1f
reynaluisina15 <reyna.luisina@gmail.com>
2022-08-31 15:02:26 -0300

    Proceso de Registro/ Button edit product

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,35 @@
 const bcryptjs = require('bcryptjs');
-const {validationResult} = require('express-validator');
+const { validationResult } = require('express-validator');
+const { loadUsers, storeUsers } = require('../data/db');
 
 module.exports = {
-  login : (req,res) => {
-    return res.render("login", {title:"Perfil"})
+  login: (req, res) => {
+    return res.render("login", { title: "Perfil" })
   },
-  proccesLogin : (req, res) => {
-    return res.render("index", {title:"index"})
+  proccesLogin: (req, res) => {
+    return res.render("index", { title: "index" })
   },
-  register : (req,res) => {
-    return res.render("register", {title:"Registro"})
-  }, 
+  register: (req, res) => {
+    return res.render("register", { title: "Registro" })
+  },
+  processRegister: (req, res) => {
+
+    const { name, surname, email, password } = req.body;
+
+    let users = loadUsers();
+
+    let newUser = {
+      id: users.length > 0 ? users[users.length - 1].id + 1 : 1,
+      name: name ? name?.trim() : "",
+      surname: surname ? surname?.trim() : "",
+      email: email?.trim(),
+      password: bcryptjs.hashSync(password, 12),
+      rol: 'user',
+      avatar: "avatar.png"
+    }
+    let usersModify = [...users, newUser];
+    
+    storeUsers(usersModify);
+    return res.redirect('/users/login');
+  }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-const {login, register, proccesLogin} = require("../controllers/userController");
+const {login, register, proccesLogin, processRegister} = require("../controllers/userController");
 
 /* user. */
 router
+      /*USER REGISTER */
      .get('/register', register)
+     .post('/register', processRegister)
 
      /* user Login. */
      .get('/login', login)
