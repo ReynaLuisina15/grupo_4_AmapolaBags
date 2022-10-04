606a9a464c0fff4b989347aee604b5fd6cea55d8
reynaluisina15 <reyna.luisina@gmail.com>
2022-10-04 14:42:30 -0300

    modif config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: "root",
-    password: null,
-    database: "database_development",
-    host: "127.0.0.1",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
   },
   test: {
