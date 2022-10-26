9ed0b8dd477a2be6bb7de3d39d05a35aa3d50206
CalebRR <yohannagraciano@gmail.com>
2022-10-26 07:46:21 -0300

    edit img and color

--- src/routes/products.js
+++ src/routes/products.js
@@ -20,7 +20,7 @@ router
      .get('/productDetail/:id', detail)
 
      .get('/productEdit/:id',adminUserCheck, edit)
-     .put('/update/:id',adminUserCheck, uploadImageProduct.single("img"),productsEditValidator, update)
+     .put('/update/:id',adminUserCheck, uploadImageProduct.array("img"),productsEditValidator, update)
 
   .get("/productDetail/:id", detail)
 
