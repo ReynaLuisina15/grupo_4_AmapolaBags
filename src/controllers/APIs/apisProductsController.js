563de691c08799f635af7726ff596db532afc15b
CalebRR <yohannagraciano@gmail.com>
2022-11-02 12:00:31 -0300

    create api: controller, router, app

---
+++ src/controllers/APIs/apisProductsController.js
@@ -0,0 +1,28 @@
+const controller = {
+    // GET IMAGE IN VIEW
+    image: (req,res) => {
+
+    },
+    // ALL PRODUCTS + QUERIES
+    all: (req,res) => {
+
+    },
+    // DETAIL PRODUCT
+    detail: (req,res) => {
+
+    },
+    // CREATE PRODUCT
+    store: (req,res) => {
+
+    },
+    // EDIT PRODUCT
+    update: (req,res) => {
+
+    },
+    // DELETE PRODUCT
+    destroy: (req,res) => {
+        
+    },
+};
+
+module.exports = controller;
---
+++ src/routes/APIs/apiProducts.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+// -------- CONTROLLER REQUIRE
+const {
+    
+} = require("../../controllers/APIs/apisProductsController");
+
+ router
+    // .get("/img/:img", image)
+    // .get("/", all)
+    // .get("/:id", detail)
+    // .post("/,store")
+    // .patch("/:id",update)
+    // .delete("/:id,destroy")
+
+module.exports = router    
