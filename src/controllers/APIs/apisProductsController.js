e3faf6bee3dc9cfa67d7b5f30669db9a8aff51a3
CalebRR <yohannagraciano@gmail.com>
2022-12-10 17:38:51 -0300

    add filter category api

--- src/controllers/APIs/apisProductsController.js
+++ src/controllers/APIs/apisProductsController.js
@@ -92,6 +92,7 @@ const controller = {
                         description:{
                             [Op.substring]:search
                         }
+                        
                     }
                 ]
             }
