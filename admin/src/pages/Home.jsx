6152c2dc0d39dea6a725446e66b2b670451da1ff
CalebRR <yohannagraciano@gmail.com>
2022-12-14 12:23:56 -0300

    add extructura React

--- admin/src/pages/Home.jsx
+++ admin/src/pages/Home.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import {Metrics} from '../components/metricas/Metrics'
+import { Categories } from '../components/categories/Categories';
 
 export const Home = () => {
   return (
@@ -8,58 +10,7 @@ export const Home = () => {
     </div>
 
 
-    <div className="row">
-
-    
-        <div className="col-md-4 mb-4">
-            <div className="card border-left-primary shadow h-100 py-2">
-                <div className="card-body">
-                    <div className="row no-gutters align-items-center">
-                        <div className="col mr-2">
-                            <div className="text-xs font-weight-bold text-primary text-uppercase mb-1">Movies in Data Base</div>
-                            <div className="h5 mb-0 font-weight-bold text-gray-800">21</div>
-                        </div>
-                        <div className="col-auto">
-                            <i className="fas fa-film fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div className="col-md-4 mb-4">
-            <div className="card border-left-success shadow h-100 py-2">
-                <div className="card-body">
-                    <div className="row no-gutters align-items-center">
-                        <div className="col mr-2">
-                            <div className="text-xs font-weight-bold text-success text-uppercase mb-1"> Total awards</div>
-                            <div className="h5 mb-0 font-weight-bold text-gray-800">79</div>
-                        </div>
-                        <div className="col-auto">
-                            <i className="fas fa-award fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div className="col-md-4 mb-4">
-            <div className="card border-left-warning shadow h-100 py-2">
-                <div className="card-body">
-                    <div className="row no-gutters align-items-center">
-                        <div className="col mr-2">
-                            <div className="text-xs font-weight-bold text-warning text-uppercase mb-1">Actors quantity
-                            </div>
-                            <div className="h5 mb-0 font-weight-bold text-gray-800">49</div>
-                        </div>
-                        <div className="col-auto">
-                            <i className="fas fa-user fa-2x text-gray-300"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <Metrics/>
 
     
 
@@ -82,89 +33,8 @@ export const Home = () => {
         </div>
         
 
+        <Categories/>
         
-        <div className="col-lg-6 mb-4">						
-            <div className="card shadow mb-4">
-                <div className="card-header py-3">
-                    <h5 className="m-0 font-weight-bold text-gray-800">Genres in Data Base</h5>
-                </div>
-                <div className="card-body">
-                    <div className="row">
-                        <div className="col-lg-6 mb-4">
-                            <div className="card bg-dark text-white shadow">
-                                <div className="card-body">
-                                    Acción
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 mb-4">
-                            <div className="card bg-dark text-white shadow">
-                                <div className="card-body">
-                                    Animación
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 mb-4">
-                            <div className="card bg-dark text-white shadow">
-                                <div className="card-body">
-                                    Aventura
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 mb-4">
-                            <div className="card bg-dark text-white shadow">
-                                <div className="card-body">
-                                    Ciencia Ficción
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 mb-4">
-                            <div className="card bg-dark text-white shadow">
-                                <div className="card-body">
-                                    Comedia
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 mb-4">
-                            <div className="card bg-dark text-white shadow">
-                                <div className="card-body">
-                                    Documental
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 mb-4">
-                            <div className="card bg-dark text-white shadow">
-                                <div className="card-body">
-                                    Drama
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 mb-4">
-                            <div className="card bg-dark text-white shadow">
-                                <div className="card-body">
-                                    Fantasia
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 mb-4">
-                            <div className="card bg-dark text-white shadow">
-                                <div className="card-body">
-                                    Infantiles
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-6 mb-4">
-                            <div className="card bg-dark text-white shadow">
-                                <div className="card-body">
-                                    Musical
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 </div>
   )
