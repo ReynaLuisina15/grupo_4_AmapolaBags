fb8fd6c0e804abac99dc537f0a2b559e2ee124b5
CalebRR <yohannagraciano@gmail.com>
2022-12-21 14:02:53 -0300

    add - items cart

--- public/javascript/orderCarts.js
+++ public/javascript/orderCarts.js
@@ -56,8 +56,72 @@ const addCartItem = async (productId) => {
       },
     });
     let result = await response.json();
-    console.log(result);
+    if (result.ok) {
+      const { items } = result.data;
+      if (items?.length) {
+        items.forEach(({ name, price, id, quantity, imgPrimary }) => {
+          const existProduct = document.querySelector(`.item${id}`)
+          if(existProduct){
+            boxCart.removeChild(existProduct)
+          }
+
+          const structure = `
+          <tr class="item${id}">
+            <td><img style="width:100px" src="/img/${imgPrimary}" alt="${name}"></td>
+            <td>${name}</td>
+            <td>
+              <div class="d-flex">
+                <button class="btn btn-sm btn-danger" onclick="removeCartItem('${id}')"><i class="fas fa-minus"></i></button>
+                <input type="text" style="border: none; width:20px; text-align:center;" value="${quantity}">
+                <button class="btn btn-sm btn-success" onclick="addCartItem('${id}')"><i class="fas fa-plus"></i></button>
+              </div>
+            </td>
+            <td> ${price}</td>
+            <td> ${price * quantity}</td>
+            <td>
+              <button class="btn btn-sm bn-danger" onclick="removeCartItemFull('${id}')"><i class="fas fa-trash"></i></button>
+            </td>
+          </tr>
+          `;
+          boxCart.innerHTML += structure;
+        });
+      } else {
+        boxCart.innerHTML = `<p class="alert alert-warning">Aún no has agregado productos</p>`;
+      }
+    }
   } catch (error) {
     console.error;
   }
 };
+const removeCartItem = async (productId) => {
+
+  try {
+      let response = await fetch('/api/carts',{
+          method : 'DELETE',
+          body : JSON.stringify({
+              productId,
+          }),
+          headers : {
+              "Content-Type" : "application/json"
+          }
+      });
+
+      let result = await response.json();
+
+      if(result.ok){
+          const {items} = result.data;
+          showCart(items)
+      }        
+
+  } catch (error) {
+      console.error
+
+  }
+};
+const removeCartItemFull = async (productId) => {
+  try {
+    
+  } catch (error) {
+    console.error;
+  }
+}
--- src/controllers/APIs/apiCartsController.js
+++ src/controllers/APIs/apiCartsController.js
@@ -91,6 +91,71 @@ module.exports = {
       });
     }
   },
-  removeItem: async (req, res) => {},
-  removeAllItem: async (req, res) => {},
+  removeItem : async(req,res) => {
+
+    try {
+
+        const {productId} = req.body;
+
+        let item = req.session.orderCart.items.find(item => item.product.id === +productId);
+
+        if(item.quantity === 1){
+            await db.Cart.destroy({
+                where : {
+                    id : item.id
+                }
+            });
+
+            const itemsModify = req.session.orderCart.items.filter(element => element.id !== item.id)
+
+            req.session.orderCart = {
+                ...req.session.orderCart,
+                items: itemsModify
+            }        
+
+        }else {
+            await db.Cart.update(
+                {
+                    quantity : item.quantity - 1
+                },
+                {
+                    where : {
+                        id : item.id
+                    }
+                }
+            )
+
+            const itemsModify = req.session.orderCart.items.map(element => {
+                if(element.id === item.id ){
+                    element.quantity = element.quantity - 1;
+                    return element
+                }
+                return element
+            })
+
+            req.session.orderCart = {
+                ...req.session.orderCart,
+                items : itemsModify
+            }        
+        }
+
+        
+
+        return res.status(200).json({
+            ok : true,
+            data : req.session.orderCart || null
+        })
+
+        
+    } catch (error) {
+        return res.status(error.status || 500).json({
+            ok : false,
+            msg : error.message || 'Upps, un error!'
+        });
+    }
+
+},
+  removeAllItem: async (req, res) => {
+
+  },
 };
