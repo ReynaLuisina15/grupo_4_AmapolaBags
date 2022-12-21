e52a0596340a68e76bb1f1c6d89b0fa53f94dbcd
Eric <menaericdaniel@gmail.com>
2022-12-21 17:35:51 -0300

    carrito funcional!

--- public/javascript/orderCarts.js
+++ public/javascript/orderCarts.js
@@ -1,47 +1,37 @@
 const bag = document.getElementById("bag");
-const boxCart = document.getElementById("box-cart");
+const boxCart = document.getElementById('box-cart-modal')
 
-bag.addEventListener("click", async () => {
-  try {
-    let response = await fetch("/api/cart");
-    let result = await response.json();
-    if (result.ok) {
-      const { items } = result.data;
-      if (items?.length) {
-        items.forEach(({ name, price, id, quantity, imgPrimary }) => {
-          const existProduct = document.querySelector(`.item${id}`)
-          if(existProduct){
-            boxCart.removeChild(existProduct)
-          }
+const showCart = (items) => {
+  boxCart.innerHTML = null;
 
-          const structure = `
-          <tr class="item${id}">
-            <td><img style="width:100px" src="/img/${imgPrimary}" alt="${name}"></td>
-            <td>${name}</td>
-            <td>
-              <div class="d-flex">
-                <button class="btn btn-sm btn-danger" onclick="removeCartItem('${id}')"><i class="fas fa-minus"></i></button>
-                <input type="text" style="border: none; width:20px; text-align:center;" value="${quantity}">
-                <button class="btn btn-sm btn-success" onclick="addCartItem('${id}')"><i class="fas fa-plus"></i></button>
-              </div>
-            </td>
-            <td> ${price}</td>
-            <td> ${price * quantity}</td>
-            <td>
-              <button class="btn btn-sm bn-danger" onclick="removeCartItemFull('${id}')"><i class="fas fa-trash"></i></button>
-            </td>
-          </tr>
-          `;
-          boxCart.innerHTML += structure;
-        });
-      } else {
-        boxCart.innerHTML = `<p class="alert alert-warning">Aún no has agregado productos</p>`;
-      }
-    }
-  } catch (error) {
-    console.error;
-  }
-});
+  items.forEach(({ name, price, id, quantity, imgPrimary }) => {
+    /*   const existProduct = document.querySelector(`.item${id}`)
+      if(existProduct){
+        boxCart.removeChild(existProduct)
+      } */
+
+      const structure = `
+      <tr class="item${id}">
+        <td><img style="width:100px" src="/img/${imgPrimary}" alt="${name}"></td>
+        <td>${name}</td>
+        <td>
+          <div class="d-flex">
+            <button class="btn btn-sm btn-danger" onclick="removeCartItem('${id}')"><i class="fas fa-minus"></i></button>
+            <input type="text" style="border: none; width:20px; text-align:center;" value="${quantity}">
+            <button class="btn btn-sm btn-success" onclick="addCartItem('${id}')"><i class="fas fa-plus"></i></button>
+          </div>
+        </td>
+        <td> ${price}</td>
+        <td> ${price * quantity}</td>
+        <td>
+          <button class="btn btn-sm bn-danger" onclick="removeCartItemFull('${id}')"><i class="fas fa-trash"></i></button>
+        </td>
+      </tr>
+      `;
+      boxCart.innerHTML += structure;
+    });
+}
+
 
 const addCartItem = async (productId) => {
   console.log("#####", productId);
@@ -58,33 +48,9 @@ const addCartItem = async (productId) => {
     let result = await response.json();
     if (result.ok) {
       const { items } = result.data;
+      console.log(items);
       if (items?.length) {
-        items.forEach(({ name, price, id, quantity, imgPrimary }) => {
-          const existProduct = document.querySelector(`.item${id}`)
-          if(existProduct){
-            boxCart.removeChild(existProduct)
-          }
-
-          const structure = `
-          <tr class="item${id}">
-            <td><img style="width:100px" src="/img/${imgPrimary}" alt="${name}"></td>
-            <td>${name}</td>
-            <td>
-              <div class="d-flex">
-                <button class="btn btn-sm btn-danger" onclick="removeCartItem('${id}')"><i class="fas fa-minus"></i></button>
-                <input type="text" style="border: none; width:20px; text-align:center;" value="${quantity}">
-                <button class="btn btn-sm btn-success" onclick="addCartItem('${id}')"><i class="fas fa-plus"></i></button>
-              </div>
-            </td>
-            <td> ${price}</td>
-            <td> ${price * quantity}</td>
-            <td>
-              <button class="btn btn-sm bn-danger" onclick="removeCartItemFull('${id}')"><i class="fas fa-trash"></i></button>
-            </td>
-          </tr>
-          `;
-          boxCart.innerHTML += structure;
-        });
+        showCart(items)
       } else {
         boxCart.innerHTML = `<p class="alert alert-warning">Aún no has agregado productos</p>`;
       }
@@ -96,7 +62,7 @@ const addCartItem = async (productId) => {
 const removeCartItem = async (productId) => {
 
   try {
-      let response = await fetch('/api/carts',{
+      let response = await fetch('/api/cart',{
           method : 'DELETE',
           body : JSON.stringify({
               productId,
@@ -108,9 +74,20 @@ const removeCartItem = async (productId) => {
 
       let result = await response.json();
 
+     
+
       if(result.ok){
           const {items} = result.data;
-          showCart(items)
+        console.log(items);
+          if (result.ok) {
+            const { items } = result.data;
+            console.log(items);
+            if (items?.length) {
+              showCart(items)
+            } else {
+              boxCart.innerHTML = `<p class="alert alert-warning">Aún no has agregado productos</p>`;
+            }
+          }
       }        
 
   } catch (error) {
@@ -119,9 +96,57 @@ const removeCartItem = async (productId) => {
   }
 };
 const removeCartItemFull = async (productId) => {
+  console.log(productId);
   try {
-    
+    let response = await fetch('/api/cart/all/' + productId,{
+        method : 'DELETE',
+        body : JSON.stringify({
+            productId,
+        }),
+        headers : {
+            "Content-Type" : "application/json"
+        }
+    });
+
+    let result = await response.json();
+
+   
+
+    if(result.ok){
+        const {items} = result.data;
+      console.log(items);
+        if (result.ok) {
+          const { items } = result.data;
+          console.log(items);
+          if (items?.length) {
+            showCart(items)
+          } else {
+            boxCart.innerHTML = `<p class="alert alert-warning">Aún no has agregado productos</p>`;
+          }
+        }
+    }        
+
+} catch (error) {
+    console.error
+
+}
+}
+
+
+bag.addEventListener("click", async () => {
+
+  try {
+    let response = await fetch("/api/cart");
+    let result = await response.json();
+    if (result.ok) {
+      const { items } = result.data;
+      if (items?.length) {
+        showCart(items)
+      } else {
+        boxCart.innerHTML = `<p class="alert alert-warning">Aún no has agregado productos</p>`;
+      }
+    }
   } catch (error) {
     console.error;
   }
-}
+});
