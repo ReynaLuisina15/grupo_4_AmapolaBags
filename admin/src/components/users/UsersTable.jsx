fb44b8a8225795f3d9fa5de5671c754830660628
facundo <facundoarevalos04@gmail.com>
2022-12-22 11:03:17 -0300

    forma basica userList

---
+++ admin/src/components/users/UsersTable.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+export const UsersTable = () => {
+    return (
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Facundo</td>
+      <td>Arévalos</td>
+      <td>facundoarevalos04@gmail.com</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+    )
+}
--- admin/src/pages/Users.jsx
+++ admin/src/pages/Users.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { UsersTable } from '../components/users/UsersTable';
+import { UseFetch } from "../hooks/UseFetch";
 
 export const Users = () => {
+  const [state, setState] = useState({});
+
+  useEffect(() => {
+    UseFetch('/users')
+      .then(({meta, data})=> {
+        if (meta.ok = true) {
+          setState(data)
+        }
+      })
+  },[])
   return (
-    <div>Users</div>
+    <div className='container'>
+      <div className='card'>
+        <div className='card-body'>
+          <UsersTable/>
+        </div>
+      </div>
+    </div>
   )
 }
