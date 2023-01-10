30de40746a594edfe0971571616850016a75617f
meligimenez <melinagimenez.dev@gmail.com>
2023-01-09 22:38:04 -0300

    update style  home

--- admin/src/components/categories/Categories.jsx
+++ admin/src/components/categories/Categories.jsx
@@ -21,7 +21,7 @@ export const Categories = () => {
                 })
             }).catch(error => console.error(error))
 
-    },[]);
+    }, []);
 
 
 
@@ -29,7 +29,7 @@ export const Categories = () => {
         <div className="col-lg-6 mb-4">
             <div className="card shadow mb-4">
                 <div className="card-header py-3">
-                    <h5 className="m-0 font-weight-bold text-gray-800">Genres in Data Base</h5>
+                    <h5 className="m-0 font-weight-bold text-gray-800">Categorias</h5>
                 </div>
                 <div className="card-body">
                     <div className="row">
@@ -40,7 +40,7 @@ export const Categories = () => {
                                 <p>Cargando...</p>
                                 :
                                 state.categories.map((category, index) => (
-                                    <Category {...category} key={category.name + index}/>
+                                    <Category {...category} key={category.name + index} />
                                 ))
 
 
--- admin/src/components/metricas/Metrics.jsx
+++ admin/src/components/metricas/Metrics.jsx
@@ -4,54 +4,54 @@ import { Metric } from './Metric'
 
 export const Metrics = () => {
   const [state, setState] = useState({
-    products : {
-        title : "Total productos",
-        icon : "fa-boxes",
-        value : 0,
-        color : "primary"
+    products: {
+      title: "Total productos",
+      icon: "fa-boxes",
+      value: 0,
+      color: "primary"
     },
-    users : {
-        title : "Usuarios registrados",
-        icon : "fa-users",
-        value : 0,
-        color : "success"
+    users: {
+      title: "Usuarios registrados",
+      icon: "fa-users",
+      value: 0,
+      color: "success"
     },
-    categories : {
-        title : "Categorías activas",
-        icon : "fa-folder",
-        value : 0,
-        color : "warning"
+    categories: {
+      title: "Total categorías",
+      icon: "fa-folder",
+      value: 0,
+      color: "warning"
     }
   });
 
   useEffect(() => {
-    
-      UseFetch("/totals")
-       .then(({data}) => {
+
+    UseFetch("/totals")
+      .then(({ data }) => {
         setState({
-           
-            products : {
-              ...state.products,
-              value : data.totalProducts
-            },
-            users : {
-                ...state.users,
-                value : data.totalUsers
-              },
-            categories : {
-                ...state.categories,
-                value : data.totalCategories
-              },
+
+          products: {
+            ...state.products,
+            value: data.totalProducts
+          },
+          users: {
+            ...state.users,
+            value: data.totalUsers
+          },
+          categories: {
+            ...state.categories,
+            value: data.totalCategories
+          },
         })
-       }).catch(error => console.error)
+      }).catch(error => console.error)
   }, []);
 
   return (
     <div className="row">
 
-    <Metric {...state.products}/>
-    <Metric {...state.users}/>
-    <Metric {...state.categories}/>
+      <Metric {...state.products} />
+      <Metric {...state.users} />
+      <Metric {...state.categories} />
 
     </div>
   )
